c5c96b1b5c0309f18079b20eb232b64e5bea1c1d
Abhay453564 <guptaabhayraj450@gmail.com>
2026-01-14 20:04:51 +0530

    Fix static files with Whitenoise

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -128,6 +129,9 @@ STATIC_ROOT = BASE_DIR / "staticfiles"   # (For production use, optional in deve
 
 MEDIA_URL = '/media/'   # URL to access media files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
